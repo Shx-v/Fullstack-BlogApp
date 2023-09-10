6a0ec6f7e8b82aae20bbb52d68573778b2d12112
shiv <shivranjanbharadwaj@gmail.com>
2023-09-10 21:50:34 +0530

    database connection bug finished

--- backend/index.js
+++ backend/index.js
@@ -14,7 +14,7 @@ const commentRoute=require('./routes/comments')
 //database
 const connectDB=async()=>{
     try{
-        await mongoose.connect("process.env.MONGO_URL")
+        await mongoose.connect(process.env.MONGO_URL)
         console.log("database is connected successfully!")
 
     }
